6bd08226616875e2a822008a15469e4a02074ab6
Jude Harding <judeharding@Judes-MacBook-Pro.local>
2017-04-24 11:22:44 -0400

    at 24 udpate

--- Exercise1/Exercise1_Start/main.js
+++ Exercise1/Exercise1_Start/main.js
@@ -1,4 +1,18 @@
 var firstName = "Jude";
 var lastName = "Harding";
+var date = new Date();
+
+var daySpan = function(date){
+  var startDate = new Date(date);
+  var currDate = new Date();
+  var div = 1000 * 60 * 60 * 24;
+  return (Math.floor((currDate - startDate) / div))
+}
 
 console.log("Hello " + firstName + " " + lastName + "!");
+console.log("Today is " + date);
+
+//ES6 backtick
+var stmt = `A total of ${daySpan('January 1, 2017')} days have passed since the start of this year.`;
+
+console.log(stmt);

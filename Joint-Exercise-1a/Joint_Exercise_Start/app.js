c709d8317d3c474c48bda9bc855b6d31c1a907f1
Jude Harding <jharding@genesisgroupusa.com>
2017-04-25 10:58:09 -0400

    exercise 1 files testing broken link to github

--- Joint-Exercise-1a/Joint_Exercise_Start/app.js
+++ Joint-Exercise-1a/Joint_Exercise_Start/app.js
@@ -4,7 +4,7 @@ var day = date.getDate();
 var month = date.getMonth();
 var year = date.getFullYear();
 month++;
-
+//a;lkfa;sdklfjas;d
 //print date in formation day/month/year
 console.log(day + "/" + month + "/" + year);
 console.log(`${day} / ${month} / ${year}`);

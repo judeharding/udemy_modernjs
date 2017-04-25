3e94f293e56f40ec9f177a6c53482e9711549c73
Jude Harding <jharding@genesisgroupusa.com>
2017-04-25 10:48:32 -0400

    exercise 1 files answer

--- Joint-Exercise-1a/Joint_Exercise_Start/app.js
+++ Joint-Exercise-1a/Joint_Exercise_Start/app.js
@@ -1,3 +1,10 @@
 //create date object
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+month++;
 
 //print date in formation day/month/year
+console.log(day + "/" + month + "/" + year);
+console.log(`${day} / ${month} / ${year}`);

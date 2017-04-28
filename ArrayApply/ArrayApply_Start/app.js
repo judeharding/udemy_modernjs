f8c65ff314e61c8cbdd67c2aa667d77b49fe052c
Jude Harding <jharding@genesisgroupusa.com>
2017-04-28 14:25:34 -0400

    arrays

---
+++ ArrayApply/ArrayApply_Start/app.js
@@ -0,0 +1,22 @@
+//Use the date object and print out the day of the week
+var date = new Date();
+var day = date.getDay();
+var daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+console.log("today is " + daysOfWeek[day]);
+
+// if (day === 0) {
+//     console.log("Today is Sunday.");
+// } else if (day === 1) {
+//     console.log("Today is Monday.");
+// } else if (day === 2) {
+//     console.log("Today is Monday.");
+// } else if (day === 3) {
+//     console.log("Today is Monday.");
+// } else if (day === 4) {
+//     console.log("Today is Monday.");
+// } else if (day === 5) {
+//     console.log("Today is Monday.");
+// } else if (day === 6) {
+//     console.log("Today is Monday.");
+// }

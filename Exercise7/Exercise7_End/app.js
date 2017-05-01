f9caf6b46479db5d382594067ca56cef3a228d29
Jude Harding <jharding@genesisgroupusa.com>
2017-04-30 23:54:14 -0400

    fun functions

--- Exercise7/Exercise7_End/app.js
+++ Exercise7/Exercise7_End/app.js
@@ -1,17 +1,28 @@
-//Remove all duplicates from this array
-var fruits = ["banana", "kiwi", "mango", "kiwi", "apple", "kiwi", "pear", "banana", "apple"],
-    compareVal;
+// //Remove all duplicates from this array
+// var fruits = ["banana", "kiwi", "mango", "kiwi", "apple", "kiwi", "pear", "banana", "apple"],
+//     compareVal;
+//
+// //sort the array
+// fruits.sort();
+// //console.log(fruits.toString());
+// //iterate backwards through the array comparing values
+// for (i = fruits.length - 1;i >= 0;i--) {
+//     compareVal = fruits[i];
+//     if (compareVal === fruits[i - 1]) {
+//         //console.log(compareVal, fruits[i - 1]);
+//         fruits.splice(i, 1);
+//     }
+// }
+//
+// console.log("New Array: " + fruits.toString());
 
-//sort the array
-fruits.sort();
-//console.log(fruits.toString());
-//iterate backwards through the array comparing values
-for (i = fruits.length - 1;i >= 0;i--) {
-    compareVal = fruits[i];
-    if (compareVal === fruits[i - 1]) {
-        //console.log(compareVal, fruits[i - 1]);
-        fruits.splice(i, 1);
-    }
+var randomNumber = function(min, max){
+  //changed from math.round to math.floor AND adding 1 to the max num
+    var init = Math.random();
+    var rand;
+
+    rand = init * (max - min) + min;
+    console.log(Math.floor(rand));
 }
 
-console.log("New Array: " + fruits.toString());
+randomNumber(12,15);
--- Exercise7/Exercise7_Start/app.js
+++ Exercise7/Exercise7_Start/app.js
@@ -1,22 +1,35 @@
-//Remove all duplicates from this array
-var fruits = ["banana", "kiwi", "mango", "kiwi", "apple", "kiwi", "pear", "banana", "apple"];
-var compareVal;
+// //Remove all duplicates from this array
+// var fruits = ["banana", "kiwi", "mango", "kiwi", "apple", "kiwi", "pear", "banana", "apple"];
+// var compareVal;
+//
+// // reposition array in alphabetical order
+// fruits.sort();
+// console.log(fruits);
+//
+// // loop thru the array
+// // sort thru the loop backwards (-1) so you don't have length issues
+// for (i = fruits.length - 1;i >= 0;i--) {
+//   // compare the elements
+//   compareVal = fruits[i];
+//   // compare 1 element to the element before it
+//   if (compareVal === fruits[i-1]){
+//     fruits.splice(i, 1);
+//     console.log("comparing val duplicate: " + compareVal);
+//   }
+// }
+// //print new array
+//
+// console.log("New Array: " + fruits.toString());
+-----------
+var sum = function(num1, num2){
+  var total = num1 + num2;
+  //b4 es6
+  // num1 = (num1 !== undefined) ? num1 : 0;
+  // num2 = (num2 !== undefined) ? num2 : 0;
+  // after es6 and change out the var above with this var below
+// var sum = function(num1 = 0, num2 = 0){
+  console.log(total);
+};
 
-// reposition array in alphabetical order
-fruits.sort();
-console.log(fruits);
-
-// loop thru the array
-// sort thru the loop backwards (-1) so you don't have length issues
-for (i = fruits.length - 1;i >= 0;i--) {
-  // compare the elements
-  compareVal = fruits[i];
-  // compare 1 element to the element before it
-  if (compareVal === fruits[i-1]){
-    fruits.splice(i, 1);
-    console.log("comparing val duplicate: " + compareVal);
-  }
-}
-//print new array
-
-console.log("New Array: " + fruits.toString());
+sum(5);
+console.log("HI");

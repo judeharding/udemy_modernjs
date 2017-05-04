786780d47d90fb78cad6120e46adb49cf7ad5173
Jude Harding <jharding@genesisgroupusa.com>
2017-05-04 16:56:39 -0400

    exercise 9

--- Exercise9/Exercise9_Start/app.js
+++ Exercise9/Exercise9_Start/app.js
@@ -1,2 +1,37 @@
-//Create an arrow function that will accept a number and return the number with an indication of whether it is Odd or Even. See if you can define the entire function on a single line using the ? : conditional operator.
+//Create an arrow function that will accept a number and return the number
+// with an indication of whether it is Odd or Even. See if you can define the
+// entire function on a single line using the ? : conditional operator.
 
+//GOT IT
+// var oddEven = (num1) => {
+//      num2 = num1 % 2;
+//      if (num2 ===0){
+//        console.log("The number is even!");
+//      } else {
+//        console.log("The number is odd!")
+//      }
+//      return num2;
+// };
+//
+// console.log("hi");
+// oddEven(11);
+//
+
+// on a single line
+
+// var oddEven = (num) => (num % 2) === 0 ? num + " is even" : num + " is odd";
+//
+// console.log(oddEven(10));
+// console.log(oddEven(11));
+//
+
+
+var dog = {
+  type: "collie",
+  age: 1,
+  speak: function(){
+    console.log("bark");
+    }
+  };
+
+  dog.speak(2);

bd6455765e723dfabdd8db0efc561e9f5a1c36c3
Jude Harding <jharding@genesisgroupusa.com>
2017-05-10 17:38:58 -0400

    constructors with notes

--- Constructor/Constructor_End/app.js
+++ Constructor/Constructor_End/app.js
@@ -1,8 +1,9 @@
 
 function Question(weight, cAnswer, lAnswer) {
     this.weight = weight;
-    this.correctAnswer = cAnswer;
-    this.learnerAnswer = lAnswer;
+    this.correctAnswer = cAnswer; // correct answer
+    this.learnerAnswer = lAnswer; //learners answer
+    // if the learners answer == to the correct answer, below will eval to true
     this.correct = (this.correctAnswer.toUpperCase() === this.learnerAnswer.toUpperCase());
 }
 
@@ -14,9 +15,10 @@ Question.prototype.getScore = function() {
     }
 };
 
-var questions = [],
-    finalScore = 0,
-    possibleScore = 0;
+
+var questions = [],   // creates an empty array for the answers
+    finalScore = 0,   // creates the learners score
+    possibleScore = 0; // creates the "perfect score"
 
 questions.push(new Question(1, "A", "B"));
 questions.push(new Question(1, "D", "B"));
@@ -28,5 +30,5 @@ for (let i = 0; i < questions.length; i++) {
     finalScore += questions[i].getScore();
     possibleScore += questions[i].weight;
 }
-
+// new es6 syntax
 console.log(`The score was ${finalScore} out of ${possibleScore}`);

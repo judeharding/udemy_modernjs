6c37b6e7350fb1470ae117ce36b5e6dd7bd07573
Jude Harding <jharding@genesisgroupusa.com>
2017-05-16 16:46:42 -0400

    constructor function?

--- Exercise10/Exercise10_Start/app.js
+++ Exercise10/Exercise10_Start/app.js
@@ -8,24 +8,53 @@
 to speed * stregnth
 Create one object providing values for speed and hitpoints. Let stregnth
 use default value. Then display damage per second*/
-
+//---------------------
 //Object.create
+//
+// var character = {  //creates the object literal
+//   speed: 6,        //defaults for properties
+//   strength: 8,
+//   hitpoints: 150,
+//
+//   damage: function(){     // the method function
+//     return this.speed * this.strength;
+//   }
+// };
+//
+// // the above is the prototype for any objects below that we create
+//
+//
+// var character1 = Object.create(character);    // creates an object with the character defaults
+//
+// character1.speed = 9;  // overriding the defaults in the prototype
+// character1.hitpoints = 205;
+//
+// console.log(character1.damage());
 
-var character = {
-  speed: 6,
-  strength: 8,
-  hitpoints: 150,
 
-  damage: function(){
-    return this.speed * this.strength;
+//---------------------
+//constructor function -- i missed this one
+
+function Character(speed, strength, hitpoints){
+  if (speed) {  //truthy if value provided = true if not, = false
+    this.speed = speed;
   }
+  if (strength) { //truthy if value provided = true if not, = false
+    this.strength = strength;
+  }
+  if (hitpoints) { //truthy if value provided = true if not, = false
+    this.hitpoints = hitpoints;
+  }
+}
+
+Character.prototype.speed = 6;  // sets default values
+Character.prototype.strength = 8;
+Character.prototype.hitpoints = 150;
+
+Character.prototype.damage = function(){
+  return this.speed * this.strength;
 };
 
-
-var character1 = Object.create(character);
-//
-// character1();
-character1.speed = 9;
-character1.hitpoints = 205;
+var character1 = new Character (9, null, 205); // overwrites the defaults above and the null will indicate noting being passed in.
 
 console.log(character1.damage());

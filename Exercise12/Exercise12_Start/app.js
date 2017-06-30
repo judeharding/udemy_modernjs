1e7687c6334c77d3e8d1e6f69b0d0bb3acc5544a
Jude Harding <jharding@genesisgroupusa.com>
2017-06-29 21:39:58 -0400

    finished ex. 12

--- Exercise12/Exercise12_Start/app.js
+++ Exercise12/Exercise12_Start/app.js
@@ -1,42 +1,40 @@
+
 //When the window loads, call this initialize function
 function init(event) {
-    var pages = ["page1.html", "page2.html", "page3.html"],
-        win = window,
-        locParts = win.location.href.split("/"),
-        curPage = locParts[locParts.length - 1],
-        pageCounter = 0,
-        leftArrow = document.querySelector("#leftArrow"),
-        rightArrow = document.querySelector("#rightArrow");
+    var leftArrow = document.querySelector("#leftArrow");
+    var rightArrow = document.querySelector("#rightArrow");
+    var urlParts = window.location.href.split("/");
+    var pages = ["page1.html", "page2.html", "page3.html"];
+    var currentPage = urlParts[urlParts.length-1];
+    var pageCounter = 0;
 
-    for (let i = 0;i < pages.length; i++) {
-        if (curPage === pages[i]) {
+
+    for (let i = 0; i < pages.length; i++) {
+        if (currentPage === pages[i]) {
             pageCounter = i;
         }
     }
 
-// use a FUNCTION for next and previous b/c you will use it on the CLICK and
-// the ARROW key pressed, then you can use them on the EVENT LISTNERS
-
     //Add function for going to next and previous page.
-    var goNext = function(e){
-        if (pageCounter < pages.length - 1){
+    var goNext = function(e) {
+        if (pageCounter < pages.length - 1) {
             pageCounter++;
-            win.location.assign(page[pageCounter]);
+            window.location.assign(pages[pageCounter]);
         }
     };
 
-    var goPrevious = function(e){
+    var goPrevious = function(e) {
         if (pageCounter > 0) {
             pageCounter--;
-            win.location.assign(page[pageCoutner]);
+            window.location.assign(pages[pageCounter]);
         }
     };
 
     //Add Event listeners for key and mouse click.
-    win.addEventListener('keydown', function(e){
-        if (e.keyCode == 37) { //left arrow key is 37
+    window.addEventListener('keydown', function(e) {
+        if (e.keyCode === 37) { //left arrow key
             goPrevious(e);
-        } else if (e.keyCode == 39) { //left arrow key is 39
+        } else if (e.keyCode === 39) { //Right arrow key
             goNext(e);
         }
     });
@@ -44,7 +42,5 @@ function init(event) {
     leftArrow.addEventListener("click", goPrevious);
     rightArrow.addEventListener("click", goNext);
 }
-
-
 
 window.addEventListener("load", init);
